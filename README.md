172dba0061d4d40bbe4e6273cd32bc40e2ffe423
Dmitry G. Anderson <dandgerson@gmail.com>
2021-09-11 02:54:44 +0300

    save character

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Djabba Screep is the experimental expirience with node js and roguelike style
 ## RoadMap to implementation
 
 - [ ] log the keypress
+- [x] save the characters into `src/characters/{characterName}`
--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log(process.stdin)
+import readline from 'readline'
+import fs from 'fs'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// console.log(rl)
+const character = {
+  name: '',
+}
+
+rl.question('What is the name of your Character? ', (answer) => {
+  // TODO: Log the answer in a database
+  character.name = answer
+  console.log(`The name of your Character is: ${character.name}`);
+
+  // rl.close();
+});
+
+// rl.on('line', (input) => {
+//   console.log(`Received: ${input}`);
+// });
+
+
+
+rl.on('close', () => {
+  const url = new URL(`./src/characters/${character.name}.json`, import.meta.url)
+  fs.writeFile(url, JSON.stringify(character), err => {
+    if (err) console.error(err)
+
+    console.log('The character was saved.')
+  })
+})
